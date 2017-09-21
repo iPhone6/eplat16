df8f8bf65e0de4e2624e3c7e306a00bf0fae0b76
zhangyangshun <zhangyangshun@foxmail.com>
2017-09-21 16:31:00 +0800

    小修改

--- src/test/java/com/cn/zpxt/test/demo/TestPattern.java
+++ src/test/java/com/cn/zpxt/test/demo/TestPattern.java
@@ -1,5 +1,8 @@
 package com.cn.zpxt.test.demo;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import java.util.Date;
 
 import com.alibaba.fastjson.JSON;
@@ -17,12 +20,20 @@ public class TestPattern {
 		String str = "1.099";
 		System.out.println(isNumber(str));
 		
+		Set<String> test_set=new TreeSet<>();
+//		test_set.add(null);	// Set中不能存放null，会报空指针异常
+		test_set.add("");
+		test_set.add("adb");
+		System.out.println(test_set);
+		
 		EpAtten epa=new EpAtten();
 		epa.setId(999111l);
 		epa.setEp_uid(909);
 		epa.setGps_addr("hhhh米宓模9090");
 		epa.setTime(new Date());
 		System.out.println(JSON.toJSONString(epa, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteDateUseDateFormat));
+		
+		
 		
 	}
 
